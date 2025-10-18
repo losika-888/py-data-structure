9f9d243db058dbef5a70b114d6289684635c2e0d
lzk <18680586438@163.com>
2025-10-18 15:54:33 +0800

    Add files via upload

---
+++ chapter4-字典/chapter4-字典.py
@@ -0,0 +1,8 @@
+#字典的创建
+dict = {'Name': 'Zara', 'Age': 7, 'Name': 'Manni'}
+print ("dict['Name']: ", dict['Name'])
+#可以获得字典中所有的keys和values
+dict.keys()
+dict.values()
+#用于从key查询对应的values
+dict.fromkeys("Name")
