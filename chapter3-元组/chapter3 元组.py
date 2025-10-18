7503d103616cb15a7eae29b593797a2d4834db34
lzk <18680586438@163.com>
2025-10-18 14:50:13 +0800

    Add files via upload

---
+++ chapter3-元组/chapter3 元组.py
@@ -0,0 +1,16 @@
+#元组的创建
+tup1=('physics', 'chemistry', 1997, 2000)
+tup2=(1,2,3,4,5,6,7)
+tup3="a","b","c","d"
+#空元组
+tup4=()
+#单元素元组
+tup5=(1,)
+
+# #元组的索引
+# print ("tup1[0]: ", tup1[0])
+# print ("tup2[1:5]: ", tup2[1:5])
+
+#删除整个元组
+del tup1
+print(tup1)
